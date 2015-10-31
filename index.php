dd859c77782acafd8d88dbbabc612abe6e677b58
Yo-Tina Verbraecken <Yo-Tina@Yo-Tinas-iMac.local>
2015-10-31 14:04:37 +0100

    The start
    This is a plain input form to put in your name and the amount you want
    to spend for secret santa.

---
+++ index.php
@@ -0,0 +1,12 @@
+<html lang="jp">
+<head>
+    <link src='' rel='stylesheet'>
+</head>    
+<body>
+    <form action="doSomething.php" method="post" id='frmStart' >
+        <label id='lblName' for='txtName'>名前。</label><input type='text' id='txtName'><br>
+        <label id='lblCost' for='txtCost'>いくらですか。</label><input type='text' id='txtCost'><br>
+        <input type='submit' id='submit' value='提出'>
+    </form>
+</body>
+</html>
